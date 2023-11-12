3119ae1313d35280774d34d0594bb09f05395b9d
s1111186 <s1111186@o365st.pu.edu.tw>
2023-11-12 23:07:48 +0800

    version 0: html5

--- web.py
+++ web.py
@@ -5,12 +5,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     homepage = "<h1>資管二A 411111868 賴品瑄的求職相關資訊</h1>"
-    homepage += "<a href=/mis>我的個人簡介</a><br>"
-    homepage += "<a href=/today>MIS相關工作介紹</a><br>"
-    homepage += "<a href=/welcome>職業測驗結果</a><br>"
-    homepage += "<a href=/about>求職履歷自傳</a><br>"
+    homepage += "<a href=/rwd>我的個人簡介</a><br>"
+    homepage += "<a href=/code>職業測驗結果</a><br>"
+    homepage += "<a href=/work>相關工作介紹</a><br>"
+    homepage += "<a href=/own>求職履歷自傳</a><br>"
     return homepage
 
+@app.route("/rwd")
+def rwd():
+    return render_template("rwd.html")
 
+@app.route("/code")
+def code():
+    return render_template("code.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/own")
+def own():
+    return render_template("own.html")
 if __name__ == "__main__":
-    app.run()
+   app.run()
